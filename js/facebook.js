ba94d53fc2c08a2bf17d3309ec612012301a69b4
chintanparikh <chintanparikh@gatech.edu>
2013-09-08 03:50:27 -0400

    final (almost)

--- js/facebook.js
+++ js/facebook.js
@@ -1,12 +1,47 @@
-$('.auth').click(function() {
-  FB.login(function(response) {
-   if (response.authResponse) {
-     console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-     });
-   } else {
-     console.log('User cancelled login or did not fully authorize.');
-   }
- });
-})
+$(document).ready(function() {
+	$('#connect').click(function() {
+		FB.login(function(response) {
+		  if (response.authResponse) {
+		    FB.api('/me/events', function(response) {
+		      $("#connect").toggleClass('hidden');
+		       
+		      $(response.data).each(function(_, elem)
+		      {		 		
+		      	var start = new Date(elem.start_time);
+		      	var current = new Date();
+		      	if (elem.end_time !== undefined)
+		      	{
+		      		end_time = new Date(elem.end_time);
+		      	}
+		      	else
+		      	{
+		      		end_time = 0
+		      	}
+		      	if ((current - start < 86400 && current > start) || (end_time > current && current > start))
+		      	{
+			       	FB.api(elem.id + '/picture?width=206&height=206', function(response) {
+			       	  $(".events").append('<div class="event"><a href="timeline.html?event=' + elem.id + '"><img src="' + response.data.url + '" class="thumbnail" /> <span class="description">' + elem.name.replace(/^(.{16}[^\s]*).*/, "$1") + '</span></a></div>');
+			       	  grayscale();
+			       	});
+			    }
+		      });
+
+		    });
+		   } else {
+		     console.log('User cancelled login or did not fully authorize.');
+		   }
+		}, {scope: 'user_events, read_stream'});
+    });
+	
+});
+
+function grayscale()
+{
+	$(".event img").each(function(_, img)
+	{
+		$(img).hover(function()
+		{
+            $(this).toggleClass('grayscale');
+		});
+	});
+}
